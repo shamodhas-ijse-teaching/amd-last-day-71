bb917c2af1c6cb7c1ff23ffb7308e8dcd5bbc943
shamodhas <shamodha7@gmail.com>
2026-01-14 13:45:17 +0530

    Add media library support and update permissions for photo handling

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,11 @@
-import { View, Text, Button, Alert, Image } from "react-native"
+import {
+  View,
+  Text,
+  Button,
+  Alert,
+  Image,
+  TouchableOpacity
+} from "react-native"
 import React, { useRef, useState } from "react"
 import "../global.css"
 import {
@@ -7,11 +14,15 @@ import {
   CameraType,
   BarcodeScanningResult
 } from "expo-camera"
+import * as MediaLibrary from "expo-media-library"
 
 // <Index r></Index>
 
 const index = () => {
   const [permissions, requestPermissions] = useCameraPermissions()
+  const [mediaPermission, requestMediaPermission] =
+    MediaLibrary.usePermissions()
+
   const [facing, setFacing] = useState<CameraType>("back")
   const [photo, setPhoto] = useState<string | null>(null)
 
@@ -23,15 +34,32 @@ const index = () => {
     }
     const result = await cameraRef.current.takePictureAsync()
     setPhoto(result.uri)
-
-    // 
+    const assets = await MediaLibrary.createAssetAsync(result.uri)
+    await MediaLibrary.createAlbumAsync("ExpoAppPhotos", assets, true)
   }
 
   if (!permissions?.granted) {
     return (
       <View className="flex-1 items-center justify-center">
         <Text>Camera permission is required</Text>
-        <Text onPress={requestPermissions}>Grant permission</Text>
+        <TouchableOpacity onPress={requestPermissions}>
+          <Text>Grant permission</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  //   const [mediaPermission, requestMediaPermission] =  MediaLibrary.usePermissions()
+  if (!mediaPermission?.granted) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <Text> We need permision to save photo to your gallery</Text>
+        <TouchableOpacity
+          onPress={requestMediaPermission}
+          className="bg-blue-600/80 p-2 rounded rounded-2"
+        >
+          <Text>Grant permission</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -39,14 +67,12 @@ const index = () => {
   return (
     <View className="flex-1 bg-red-50">
       {photo && (
-        <Image
-          source={{ uri: photo }}
-          style={{ width: "100%", height: "200" }}
-        />
+        <Image source={{ uri: photo }} style={{ width: "100%", height: 200 }} />
       )}
       <CameraView
         ref={cameraRef}
         style={{ flex: 1 }}
+        // className="flex-1" - not work
         facing={facing}
         barcodeScannerSettings={{
           barcodeTypes: ["qr"]
