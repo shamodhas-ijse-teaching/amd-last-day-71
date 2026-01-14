9ccbc79b7b25afda79f5721f647bc46ab23b5270
shamodhas <shamodha7@gmail.com>
2026-01-14 10:42:23 +0530

    Add expo-camera integration and handle camera permissions

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,23 @@
 import { View, Text } from "react-native"
 import React from "react"
 import "../global.css"
+import { CameraView, useCameraPermissions } from "expo-camera"
 
 const index = () => {
+  const [permissions, requestPermissions] = useCameraPermissions()
+
+  if (!permissions?.granted) {
+    return (
+      <View className="flex-1 items-center justify-center ">
+        <Text>Camera permission is required</Text>
+        <Text onPress={requestPermissions}>Grant permission</Text>
+      </View>
+    )
+  }
+
   return (
-    <View>
-      <Text>index</Text>
+    <View className="flex-1 bg-red-50">
+      <CameraView style={{ flex: 1 }} facing="back" />
     </View>
   )
 }
